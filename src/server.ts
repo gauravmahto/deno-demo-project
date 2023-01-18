662e5398f5eda44f0697888b621df58be50d3ac7
Gaurav <gaurav.mahto2@aristocrat.com>
2023-01-18 12:54:59 +0530

    Updated linting rules

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,40 @@
-import { promisify, serve } from './deps.ts'
+import { promisify, serve } from './deps.ts';
 
 function getData(cb: (time: number) => void): void {
-
   const createTimeout = () => {
-
     const time = Number(Math.random().toPrecision(2)) * 1000;
 
     const timer = setTimeout(() => {
-
       cb(time);
 
       clearTimeout(timer);
 
       createTimeout();
-
     }, time);
-
   };
 
   createTimeout();
-
 }
 
 async function* getNumbers(initialNumber = 0) {
-
   yield initialNumber;
-
 }
 
 const delayedResponses = {
-
   delays: [500, 1300, 3500],
 
   wait(delay: number): Promise<number> {
-
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(resolve, delay);
     });
-
   },
 
   async *[Symbol.asyncIterator]() {
-
     for (const delay of this.delays) {
-
       await this.wait(delay);
       yield `Delayed response for ${delay} milliseconds`;
-
     }
-
-  }
-
+  },
 };
 
 const getDataAsync = promisify(getData);
@@ -59,33 +43,25 @@ const getDataAsync = promisify(getData);
 // console.log(await getDataAsync());
 
 (async () => {
-
   for await (const message of delayedResponses) {
-
     console.log(message);
-
   }
-
 })();
 
 const BOOK_ROUTE = new URLPattern({ pathname: '/entry/:id' });
 
 function handler(req: Request): Response {
-
   const match = BOOK_ROUTE.exec(req.url);
 
   if (match) {
-
     const id = match.pathname.groups.id;
 
     return new Response(`Entry id ${id}`);
-
   }
 
   return new Response('Not found (try /entry/1)', {
     status: 404,
   });
-
 }
 
 serve(handler);
--- src/streaming.ts
+++ src/streaming.ts
@@ -1,4 +1,4 @@
-import { serve } from './deps.ts'
+import { serve } from './deps.ts';
 
 const route = new URLPattern({ pathname: '/:activity' });
 
@@ -8,86 +8,56 @@ const delay = 1_000;
 let stopStreaming = false;
 
 function* getNumbers(initialNumber = 0) {
-
   while (!stopStreaming) {
-
     yield ++initialNumber;
-
   }
-
 }
 
 function wait(delay: number): Promise<number> {
-
-  return new Promise(resolve => setTimeout(resolve, delay));
-
+  return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
 function iteratorToStream(iterator: Iterator<unknown>): ReadableStream {
-
   return new ReadableStream({
-
     async pull(controller) {
-
       const { value, done } = await iterator.next();
 
       if (done) {
-
         controller.close();
-
       } else {
-
         if (delay > 0) {
-
           await wait(delay);
-
         }
 
         const message = `It is ${value}\n`;
         controller.enqueue(new TextEncoder().encode(message));
-
       }
-
-    }
-
+    },
   });
-
 }
 
 function handler(req: Request): Response {
-
   const match = route.exec(req.url);
 
   let body;
 
   if (match) {
-
     const activity = match.pathname.groups.activity;
 
     if ('stop' === String(activity).toLowerCase()) {
-
       stopStreaming = true;
-
     } else if ('start' === String(activity).toLowerCase()) {
-
       stopStreaming = false;
-
     }
 
     return new Response(`Received request - id: ${activity}`);
-
   } else {
-
     let timer: number | undefined = undefined;
 
     if (useIteratorToStream) {
-
       body = iteratorToStream(getNumbers(0));
-
     } else {
-
       body = new ReadableStream({
-
         start(controller) {
           timer = setInterval(() => {
             const message = `It is ${new Date().toISOString()}\n`;
@@ -96,24 +66,20 @@ function handler(req: Request): Response {
         },
 
         cancel() {
-          if (timer !== undefined) {
+          if (typeof timer !== 'undefined') {
             clearInterval(timer);
           }
-        }
-
+        },
       });
-
     }
 
     return new Response(body, {
       headers: {
         'content-type': 'text/plain',
-        'x-content-type-options': 'nosniff'
+        'x-content-type-options': 'nosniff',
       },
     });
-
   }
-
 }
 
 serve(handler);
